ff1ae4cd557d3461839920da4df573ab91ffa301
“Mathew <danilevichmathew@gmail.com>
2020-11-18 16:35:39 +0300

    add task with graphs

---
+++ Graph/Graph/Graph.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+
+namespace Graph
+{
+    public class Graph
+    {
+        private List<List<int>> AdjacencyLists { get; set; }
+        public List<List<int>> ConnectivityComponents { get; }
+        private List<bool> UsedVertexes { get; set; }
+
+        public Graph(List<List<int>> adjacencyLists)
+        {
+            AdjacencyLists = new List<List<int>>(adjacencyLists);
+            ConnectivityComponents = new List<List<int>>();
+            UsedVertexes = new List<bool>();
+            
+            for (int i = 0; i < AdjacencyLists.Count; i++)
+            {
+                UsedVertexes.Add(false);
+            }
+
+            BuildConnectivityComponents();
+        }
+
+        public Graph(int[][] adjacencyMatrix)
+        {
+            AdjacencyLists = new List<List<int>>(); 
+            for (int i = 0; i < adjacencyMatrix.Length; i++)
+            {
+                AdjacencyLists.Add(new List<int>());
+                for (int j = 0; j < adjacencyMatrix[i].Length; j++)
+                {
+                    if (adjacencyMatrix[i][j] == 1)
+                    {
+                        AdjacencyLists[i].Add(j);
+                    }
+                }
+            }
+            
+            ConnectivityComponents = new List<List<int>>();
+            UsedVertexes = new List<bool>();
+            
+            for (int i = 0; i < AdjacencyLists.Count; i++)
+            {
+                UsedVertexes.Add(false);
+            }
+
+            BuildConnectivityComponents();
+        }
+
+        private void BuildConnectivityComponents()
+        {
+            for (int i = 0; i < UsedVertexes.Count; i++)
+            {
+                if (!UsedVertexes[i])
+                {
+                    List<int> currentConnectivityComponent = new List<int>();
+                    FindOneComponent(i, currentConnectivityComponent);
+                    ConnectivityComponents.Add(currentConnectivityComponent);
+                }
+            }
+
+            for (int i = 0; i < UsedVertexes.Count; i++) { UsedVertexes[i] = false; }
+        }
+
+        private void FindOneComponent(int currentVertex, List<int> currentConnectivityComponent)
+        {
+            UsedVertexes[currentVertex] = true;
+            currentConnectivityComponent.Add(currentVertex);
+
+            for (int i = 0; i < AdjacencyLists[currentVertex].Count; i++)
+            {
+                int whereDoWeGoNext = AdjacencyLists[currentVertex][i];
+                if (!UsedVertexes[whereDoWeGoNext])
+                {
+                    FindOneComponent(whereDoWeGoNext, currentConnectivityComponent);
+                }
+            }
+        }
+
+        public (List<int> firstPart, List<int> secondPart) FindBipartiteParts()
+        {
+            List<int> firstPart = new List<int>();
+            List<int> secondPart = new List<int>();
+            
+            DepthColorSearch(0, firstPart, secondPart);
+            if (firstPart.Count + secondPart.Count != AdjacencyLists.Count)
+                return (null, null);
+            
+            foreach (int vertex in firstPart)
+                if (secondPart.Contains(vertex))
+                    return (null, null);
+
+            return (firstPart, secondPart);
+        }
+        
+        private void DepthColorSearch(int currentVertex, List<int> firstColorVertexes, List<int> secondColorVertexes, bool isFirstColor = true)
+        {
+            UsedVertexes[currentVertex] = true;
+            
+            if (isFirstColor)
+                firstColorVertexes.Add(currentVertex);
+            else
+                secondColorVertexes.Add(currentVertex);
+
+            for (int i = 0; i < AdjacencyLists[currentVertex].Count; i++)
+            {
+                int nextVertex = AdjacencyLists[currentVertex][i];
+                if (!UsedVertexes[nextVertex])
+                    DepthColorSearch(nextVertex, firstColorVertexes, secondColorVertexes, !isFirstColor);
+            }
+        }
+    }
+}
---
+++ Graph/Graph/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Graph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GraphWithComponentsDemo();
+            BipartiteDemo();
+        }
+
+        private static void GraphWithComponentsDemo()
+        {
+            Console.Out.WriteLine("Demo with connectivity components:");
+
+            int[][] adjacencyMatrix = {
+                new[]{0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                new[]{1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                new[]{1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+                new[]{0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0},
+                new[]{0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+                new[]{0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+                new[]{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0},
+                new[]{0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+                new[]{0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0},
+                new[]{0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                new[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+            };
+            
+            Graph graphWithComponents = new Graph(adjacencyMatrix);
+            
+            PrintConnectivityComponents(graphWithComponents.ConnectivityComponents);
+            
+            PrintIsGraphBipartite(graphWithComponents);
+        }
+
+        private static void BipartiteDemo()
+        {
+            Console.Out.WriteLine("Demo with bipartite graph");
+
+            int[][] adjacencyMatrix = {
+                new[]{0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+                new[]{0, 0, 0, 0, 0, 1, 0, 0, 1, 0},
+                new[]{0, 0, 0, 0, 0, 1, 0, 1, 0, 0},
+                new[]{0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+                new[]{0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+                new[]{0, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+                new[]{1, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                new[]{0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+                new[]{0, 1, 0, 1, 0, 0, 0, 0, 0, 0},
+                new[]{1, 0, 0, 1, 1, 0, 0, 0, 0, 0}
+                
+            };
+            
+            Graph bipartiteGraph = new Graph(adjacencyMatrix);
+            
+            PrintConnectivityComponents(bipartiteGraph.ConnectivityComponents);
+            
+            PrintIsGraphBipartite(bipartiteGraph);
+        }
+        
+        private static void PrintConnectivityComponents(List<List<int>> connectivityComponents)
+        {
+            Console.WriteLine("===========");
+            Console.WriteLine($"Connectivity Components.");
+
+            Console.WriteLine ($"The largest component is of size {connectivityComponents.Max(component => component.Count)}");
+            
+            Console.WriteLine($"Total Amount is {connectivityComponents.Count}");
+            for (int i = 0; i < connectivityComponents.Count; i++)
+            {
+                StringBuilder sb = new StringBuilder("");
+
+                sb.Append($"Component {i} - {{ ");
+                foreach (int currentVertex in connectivityComponents[i])
+                    sb.Append($"{currentVertex}, ");
+
+                sb.Remove(sb.Length - 2, 1);
+                sb.Append("}");
+                Console.WriteLine(sb.ToString());
+            }
+            Console.WriteLine("===========");
+        }
+
+        private static void PrintIsGraphBipartite(Graph graph)
+        {
+            Console.WriteLine("===========");
+            (List<int> firstPart, List<int> secondPart) = graph.FindBipartiteParts();
+            
+            if (firstPart != null && secondPart != null)
+            {
+                Console.WriteLine("Graph is bipartite");
+                
+                Console.Write("First part: ");
+                foreach (int vertex in firstPart)
+                {
+                    Console.Write($"{vertex} ");
+                }
+            
+                Console.WriteLine();
+            
+                Console.Write("Second part: ");
+                foreach (int vertex in secondPart)
+                {
+                    Console.Write($"{vertex} ");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("This graph is not bipartite");
+            }
+            
+            Console.WriteLine("===========");
+        }
+        
+    }
+}
