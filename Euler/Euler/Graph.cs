c5e4a3e6a6f7d7e47dbc6f388dbcb01bdfcbf204
“Mathew <danilevichmathew@gmail.com>
2020-11-24 23:19:12 +0300

    add euler graph for numbers

---
+++ Euler/Euler/Graph.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Graph
+{
+    public class Graph
+    {
+        private List<List<int>> AdjacencyLists { get; set; }
+        public List<List<int>> ConnectivityComponents { get; }
+        private List<bool> UsedVertexes { get; set; }
+
+        public Graph(List<List<int>> adjacencyLists)
+        {
+            AdjacencyLists = new List<List<int>>(adjacencyLists);
+            ConnectivityComponents = new List<List<int>>();
+            UsedVertexes = new List<bool>();
+            
+            for (int i = 0; i < AdjacencyLists.Count; i++)
+            {
+                UsedVertexes.Add(false);
+            }
+
+            BuildConnectivityComponents();
+        }
+
+        public Graph(int[][] adjacencyMatrix)
+        {
+            AdjacencyLists = new List<List<int>>(); 
+            for (int i = 0; i < adjacencyMatrix.Length; i++)
+            {
+                AdjacencyLists.Add(new List<int>());
+                for (int j = 0; j < adjacencyMatrix[i].Length; j++)
+                {
+                    if (adjacencyMatrix[i][j] == 1)
+                    {
+                        AdjacencyLists[i].Add(j);
+                    }
+                }
+            }
+            
+            ConnectivityComponents = new List<List<int>>();
+            UsedVertexes = new List<bool>();
+            
+            for (int i = 0; i < AdjacencyLists.Count; i++)
+            {
+                UsedVertexes.Add(false);
+            }
+
+            BuildConnectivityComponents();
+        }
+
+        public Graph(string[] words)
+        {
+            AdjacencyLists = new List<List<int>>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                AdjacencyLists.Add(new List<int>());
+                char lastCharOfWord = words[i].Last();
+                for (int j = 0; j < words.Length; j++)
+                {
+                    if (words[j].First() == lastCharOfWord && i != j)
+                    {
+                        AdjacencyLists[i].Add(j);
+                    }
+                }
+            }
+            
+            ConnectivityComponents = new List<List<int>>();
+            UsedVertexes = new List<bool>();
+            
+            for (int i = 0; i < AdjacencyLists.Count; i++)
+            {
+                UsedVertexes.Add(false);
+            }
+
+            BuildConnectivityComponents();
+        }
+
+        private void BuildConnectivityComponents()
+        {
+            for (int i = 0; i < UsedVertexes.Count; i++)
+            {
+                if (!UsedVertexes[i])
+                {
+                    List<int> currentConnectivityComponent = new List<int>();
+                    FindOneComponent(i, currentConnectivityComponent);
+                    ConnectivityComponents.Add(currentConnectivityComponent);
+                }
+            }
+
+            for (int i = 0; i < UsedVertexes.Count; i++) { UsedVertexes[i] = false; }
+        }
+
+        private void FindOneComponent(int currentVertex, List<int> currentConnectivityComponent)
+        {
+            UsedVertexes[currentVertex] = true;
+            currentConnectivityComponent.Add(currentVertex);
+
+            for (int i = 0; i < AdjacencyLists[currentVertex].Count; i++)
+            {
+                int whereDoWeGoNext = AdjacencyLists[currentVertex][i];
+                if (!UsedVertexes[whereDoWeGoNext])
+                {
+                    FindOneComponent(whereDoWeGoNext, currentConnectivityComponent);
+                }
+            }
+        }
+        
+        public bool IsEuler()
+        {
+            foreach (var vertexList in AdjacencyLists)
+            {
+                if (vertexList.Count % 2 != 0)
+                    return false;
+            }
+
+            return ConnectivityComponents.Count == 1;
+        }
+        
+        public Stack<int> EulerCycle()
+        {
+            if (!IsEuler())
+            {
+                return null;
+            }
+            
+            Stack<int> tempStack = new Stack<int>();
+            Stack<int> eulerStack = new Stack<int>();
+            
+            List<List<int>> graphCopy = new List<List<int>>(AdjacencyLists);
+            
+            int vertex = 0;
+
+            tempStack.Push(vertex);
+            
+            while (tempStack.Count > 0)
+            {
+                vertex = tempStack.Peek();
+
+                if (graphCopy[vertex].Count > 0)
+                {
+                    int nextVertex = graphCopy[vertex][0];
+
+                    tempStack.Push(nextVertex);
+                    
+                    graphCopy[vertex].Remove(nextVertex);
+                    graphCopy[nextVertex].Remove(vertex);
+                }
+                else
+                {
+                    int vertexToEuler = tempStack.Pop();
+                    eulerStack.Push(vertexToEuler);
+                }
+            }
+
+            return eulerStack;
+        }
+    }
+}
---
+++ Euler/Euler/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Graph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EulerDemo();
+        }
+        
+        private static void EulerDemo()
+        {
+            Console.Out.WriteLine("Demo with Euler graph");
+
+            int[][] vertexesWithEulerCycle =
+            {
+                new []{1, 3},
+                new []{0, 2, 3, 4},
+                new []{1, 4},
+                new []{0, 1, 4, 5},
+                new []{1, 2, 3, 6},
+                new []{3, 6},
+                new []{4, 5},
+            };
+            
+            List<List<int>> eulerGraphList = new List<List<int>>();
+
+            foreach (int[] vertex in vertexesWithEulerCycle)
+            {
+                eulerGraphList.Add(new List<int>(vertex));
+            }
+            
+            //string[] words =
+            //{
+            //    "киев", //2
+            //    "воронеж", //1
+            //    "жданов", //2
+            //    "витебск", //2
+            //    "каранганда", //1
+            //    "архангельск" //2
+            //};
+            
+            Graph eulerGraph = new Graph(eulerGraphList);
+
+            PrintEulerInfo(eulerGraph);
+        }
+
+        private static void PrintEulerInfo(Graph graph)
+        {
+            Console.WriteLine("===========");
+            if (graph.IsEuler())
+            {
+                Console.WriteLine("Graph is Euler");
+                
+                Stack<int> eulerCycle = graph.EulerCycle();
+                
+                Console.Write("Euler cycle: ");
+                while (eulerCycle.Count != 0)
+                {
+                    Console.Write($"{eulerCycle.Pop()} ");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Graph is not Euler");
+            }
+            Console.WriteLine("===========");
+        }
+    }
+}
